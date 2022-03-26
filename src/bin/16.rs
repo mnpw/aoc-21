7db70cd0a8db0136b2392a3b4f4cbc05ecddecc2
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2022-03-26 11:54:59 +0530

    Add day 16 sol part 2

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -2,17 +2,27 @@ use std::collections::VecDeque;
 
 fn main() {
     let input = std::fs::read_to_string("input/16").unwrap();
-    part1(&input);
+    let packet = parse_input(&input);
+    part1(&packet);
+    part2(&packet);
 }
 
-fn part1(input: &str) {
+fn parse_input(input: &str) -> Packet {
     let binary_data = input
+        .trim()
         .chars()
         .map(|ch| format!("{:04b}", ch.to_digit(16).unwrap()))
         .collect::<String>();
 
-    let packet = Packet::parse(&binary_data);
+    Packet::parse(&binary_data)
+}
+
+fn part1(packet: &Packet) {
     println!("Part 1 sol {}", packet.get_versions().iter().sum::<i64>());
+}
+
+fn part2(packet: &Packet) {
+    println!("Part 2 sol {}", packet.get_value().unwrap());
 }
 
 #[derive(Debug)]
@@ -41,7 +51,7 @@ impl Packet {
 
         if type_id == 4 {
             // literal type
-            let (val, sz) = Packet::parse_literal_value(&transmission[curr_idx..]);
+            let (val, sz) = Packet::get_literal_value(&transmission[curr_idx..]);
             curr_idx += sz;
 
             literal_value = Some(val);
@@ -90,11 +100,24 @@ impl Packet {
         }
     }
 
-    fn parse_literal_value(literal: &str) -> (i64, usize) {
+    fn get_literal_value(transmission_substr: &str) -> (i64, usize) {
+        // parse the transmission substring and
+        // convert the prefixed binary representation
+        // to the decimal value
+        //
+        // prefixed binary representation: (_ for emphasis)
+        // _1_0111_1_1110_0_0101
+        //
+        // binary value:
+        // 011111100101
+        //
+        // decimal value:
+        // 2021
+
         let mut literal_value = String::new();
         let mut literal_value_len: usize = 0;
 
-        let vec_of_strings = literal
+        let vec_of_strings = transmission_substr
             .chars()
             .map(|c| c.to_string())
             .collect::<Vec<String>>();
@@ -114,6 +137,12 @@ impl Packet {
     }
 
     fn get_versions(&self) -> Vec<i64> {
+        // parse the packet tree (bfs style)
+        //
+        // create add version number of every packet to a list
+        //
+        // return the list
+
         let mut version_list = Vec::new();
         let mut queue = VecDeque::new();
 
@@ -130,6 +159,44 @@ impl Packet {
         }
 
         version_list
+    }
+
+    fn get_value(&self) -> Option<i64> {
+        // parse the packet tree (dfs style)
+        //
+        // apply the operations specified in the parent
+        // packet on all the child packets
+        //
+        // return the result of applying all operations
+
+        if self.type_id != 4 {
+            let child_packets = self.subpackets.as_ref().unwrap();
+            let mut child_values = Vec::new();
+            for p in child_packets {
+                child_values.push(p.get_value().unwrap());
+            }
+
+            let result = child_values
+                .into_iter()
+                .reduce(|acc, item| Packet::apply_op(self.type_id, acc, item));
+
+            return result;
+        } else {
+            return self.literal_value;
+        }
+    }
+
+    fn apply_op(type_id: i64, x: i64, y: i64) -> i64 {
+        match type_id {
+            0 => x + y,
+            1 => x * y,
+            2 => std::cmp::min(x, y),
+            3 => std::cmp::max(x, y),
+            5 => (x > y) as i64,
+            6 => (x < y) as i64,
+            7 => (x == y) as i64,
+            _ => 0,
+        }
     }
 }
 
